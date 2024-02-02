1ef380b8666c587962c2c1db4329ca983cd86692
Alexander <alexanderls212@gmail.com>
2024-02-01 23:55:40 -0600

    Building login screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/Login';
+import './App.css';
+import { useEffect, useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-    <h1>hello world</h1>
-    <h4>React Music App</h4>
-    </>
-  )
-}
-
-export default App
