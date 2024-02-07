5513b291192421024c3055cbcacf09648013a01c
Alexander <alexanderls212@gmail.com>
2024-02-06 23:57:46 -0600

    Adding style

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 
 const Login = (props) => {
   const [email, setEmail] = useState('')
-  const [password, setPasword] = useState('')
+  const [password, setPassword] = useState('')
   const [emailError, setEmailError] = useState('')
   const [passwordError, setPasswordError] = useState('')
 
@@ -25,7 +25,16 @@ const Login = (props) => {
           placeholder='Enter your email here'
           onChange={ev => setEmail(ev.target.value)}
           className={'inputBox'} />
-        <label className='errLabel'>{passwordError}</label>
+        <label className='errorLabel'>{emailError}</label>
+      </div>
+      <br />
+      <div className={'inputContainer'}>
+        <input 
+          value={password}
+          placeholder='Enter your password here'
+          onChange={ev => setPassword(ev.target.value)}
+          className={'inputBox'} />
+        <label className='errorLabel'>{passwordError}</label>
       </div>
       <br />
       <div className={'inputContainer'}>
@@ -33,7 +42,7 @@ const Login = (props) => {
           className={'inputButton'}
           type='button'
           onClick={onButtonClick}
-          value={'Log in'} />
+          value={'Log in'}/>
       </div>
     </div>
   )
