d4083c024a8cefe625e98efd2a0d5355cb73a789
Alexander <alexanderls212@gmail.com>
2024-02-08 23:54:40 -0600

    Working on server

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const Home = (props) => {
+    const { loggedIn, email } = props
+    const navigate = useNavigate();
+  
+    const onButtonClick = () => {
+      //Function for later
+    }
+  
+    return <div className='mainContainer'>
+        <div className={'titleContainer'}>
+          <div>Welcome!</div>
+        </div>
+        <div>
+          This is the home page.
+        </div>
+        <div className={'buttonContainer'}>
+          <input 
+            className={'inputButton'}
+            type='button'
+            onClick={onButtonClick}
+            value={loggedIn ? 'Log Out' : 'Log In'} />
+            {(loggedIn ? <div>
+              Your email addres is {email}
+              </div> : <div/>)}
+        </div>
+
+      </div>
+  }
+  
+  export default Home
